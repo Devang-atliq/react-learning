00408e1c0f0d5cee04062fc4ec00ce5ab67307ca
Devang <devang@atliq.com>
2024-07-15 18:04:38 +0530

    react and vite both basice setup

---
+++ 01react/src/App.js
@@ -0,0 +1,12 @@
+import Demo from "./Demo";
+
+function App() {
+  return (
+    <div>
+      <h1>Create React Module + React</h1>
+      <Demo/>
+    </div>
+  );
+}
+
+export default App;
---
+++ 01react/src/Demo.js
@@ -0,0 +1,6 @@
+function Demo(){
+    return(
+        <h4>Chai is ready</h4>
+    )
+}
+export default Demo;
